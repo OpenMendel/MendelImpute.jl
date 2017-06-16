16b4e4ffad7de87a1e8cdbb4ec2299c59629e14b
Hua Zhou <huazhou@ucla.edu>
2017-06-16 12:51:09 -0700

    sliding window seems to be working now; achieve similar error rates as Rory's Matlab code

--- src/haplotyping.jl
+++ src/haplotyping.jl
@@ -187,6 +187,7 @@ function haploimpute!(
     )
 
     obj = typemax(eltype(hapscore))
+    initmissing!(X)
     for iter in 1:maxiters
         # haplotyping
         haplopair!(X.values, H, M, N, happair, hapscore)
@@ -218,33 +219,99 @@ function haploimpute!(
     people, snps = size(X)
     haplotypes = size(H, 1)
     # no need for sliding window
-    if nsnps < 3width
+    if snps < 3width
         # TODO
     end
 
     # allocate working arrays
-    Xwork = X[:, 1:3width] # NullableMatrix
-    Xw1    = view(Xwork.values, :, 1:width)
-    Xw23   = view(Xwork.values, :, (width + 1):3width)
-    X23    = view(X.values, :, (width + 1):3width)
-    Hwork = H[:, 1:3width]
-    H1    = view(H, :, 1:width)
+    Xwork    = X[:, 1:3width] # NullableMatrix
+    Xw1      = view(Xwork.values, :, 1:width)
+    Xw23     = view(Xwork.values, :, (width + 1):3width)
+    Xwb1     = view(Xwork.isnull, :, 1:width)
+    Xwb23    = view(Xwork.isnull, :, (width + 1):3width)
+    Hwork    = view(H, :, 1:3width)
     M        = zeros(eltype(H), haplotypes, haplotypes)
     N        = zeros(promote_type(eltype(H), eltype(X.values)), people, haplotypes)
     happair  = zeros(Int, people), zeros(Int, people)
-    hapscore = zeros(eltype(N), people
+    hapscore = zeros(eltype(N), people)
+
+    # number of windows
+    windows = floor(Int, snps / width)
 
     # phase and impute window 1
     if verbose; println("Imputing SNPs 1:$width"); end
     haploimpute!(Xwork, Hwork, M, N, happair, hapscore, maxiters, tolfun)
-    fillgeno!(Xw1, H1, happair)
-    copy!(Xw23, X23)
+    fill!(Xwb1, false)
 
-    # phase and impute window 2
-    if verbose; println("Imputing SNPs $width+1:2$width"); end
+    # first 1/3: ((w - 2) * width + 1):((w - 1) * width)
+    # middle 1/3: ((w - 1) * width + 1):(w * width)
+    # last 1/3: (w * width + 1):((w + 1) * width)
+    for w in 2:(windows - 1)
+        if verbose
+            println("Imputing SNPs $((w - 1) * width + 1):$(w * width)")
+        end
+        # overwrite first 1/3 by phased haplotypes
+        H1    = view(H,        :, ((w - 2) * width + 1):((w - 1) * width))
+        X1    = view(X.values, :, ((w - 2) * width + 1):((w - 1) * width))
+        fillgeno!(X1, H1, happair)
+        copy!(Xw1, X1)
+        # refresh second and third 1/3 to original data
+        X23   = view(X.values, :, ((w - 1) * width + 1):((w + 1) * width))
+        Xb23  = view(X.isnull, :, ((w - 1) * width + 1):((w + 1) * width))
+        copy!(Xw23, X23)
+        copy!(Xwb23, Xb23)
+        # phase + impute
+        Hwork = view(H, :, ((w - 2) * width + 1):((w + 1) * width))
+        haploimpute!(Xwork, Hwork, M, N, happair, hapscore, maxiters, tolfun)
+    end
+
+    # last window
+    if verbose
+        println("Imputing SNPs $((windows - 1) * width + 1):$snps")
+    end
+    Xwork = X[:, ((windows - 2) * width + 1):snps]
+    Hwork = view(H,        :, ((windows - 2) * width + 1):snps)
+    H1    = view(H,        :, ((windows - 2) * width + 1):((windows - 1) * width))
+    X1    = view(X.values, :, ((windows - 2) * width + 1):((windows - 1) * width))
+    fillgeno!(X1, H1, happair)
+    copy!(Xw1, X1)
+    X23   = view(X.values,     :, ((windows - 1) * width + 1):snps)
+    H23   = view(H,            :, ((windows - 1) * width + 1):snps)
+    Xw23  = view(Xwork.values, :, (width + 1):size(Xwork, 2))
+    copy!(Xw23, X23)
+    Xb23  = view(X.isnull,     :, ((windows - 1) * width + 1):snps)
+    copy!(Xwb23, Xb23)
     haploimpute!(Xwork, Hwork, M, N, happair, hapscore, maxiters, tolfun)
-    fillgeno!(Xw1, H1, happair)
-    copy!(Xw23, X23)
+    fillgeno!(X23, H23, happair)
 
+    return nothing
 
+end
+
+"""
+    initmissing(X)
+
+Initialize the missing values in a nullable matrix `X` by `2 x` allele frequency.
+"""
+function initmissing!(X::NullableMatrix)
+    T = eltype(X.values)
+    for j in 1:size(X, 2)
+        # allele frequency
+        cnnz = 0
+        csum = zero(T)
+        for i in 1:size(X, 1)
+            if ~X.isnull[i, j]
+                cnnz += 1
+                csum += X.values[i, j]
+            end
+        end
+        # set missing values to 2freq
+        imp = csum / cnnz
+        for i in 1:size(X, 1)
+            if X.isnull[i, j]
+                X.values[i, j] = imp
+            end
+        end
+    end
+    return nothing
 end
