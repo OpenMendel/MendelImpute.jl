1e4bd6333fa91525d60e885d505c5efc970edb58
Unknown <benchu99@hotmail.com>
2019-08-19 11:32:34 -0700

    new data structure for holding redundant haplotype sets for each person in each window

--- src/data_structures.jl
+++ src/data_structures.jl
@@ -26,28 +26,42 @@ hash(x::Prehashed) = x.hash
 """
 Data structure for keeping track of unique haplotypes in each window. 
 
-Let `w` be the current window, then `uniqH[w]` is the unique haplotype indices
+Let `w` be the current window, then `uniqueindex[w]` is the unique haplotype indices
 of window `w`. `hapmap[w]` maps every haplotype to its unique haplotype. Once 
 the best haplotype pair (h1, h2) is identified, one can use `hapmap` to 
-generate a set of haplotypes in window `w`. 
+generate a set of redundant haplotypes in window `w`. 
 
 # Example:
 
 Suppose in window `w` there are 9 haplotypes (represented by different letters):
 
-	Hw = [a b b c b d a d a]
+	H[w, :] = [a b b c b d a d a]
 
 then
 
-	uniqH[w]  = [1 2 4 6]
+	uniqueindex[w]  = [1 2 4 6]
 	hapmap[w] = [1 2 2 4 2 6 1 6 1].
 
-If (b, c) is the optimal haplotype pair, then the set of haplotypes that matches
-can (b, c) is in columns {2, 3, 4, 5}
+`hapmap` is used to find the set of matching haplotypes after identifying the best
+haplotype pair. If (b, c) is the optimal haplotype pair in the current window, then 
+the set of haplotypes that matches (b, c) is in columns {2, 3, 4, 5}
 """
 struct UniqueHaplotypeMaps
-    uniqH::Vector{Vector{Int}}
+    uniqueindex::Vector{Vector{Int}}
     hapmap::Vector{Vector{Int}}
 end
-UniqueHaplotypeMaps(windows, haps) = UniqueHaplotypeMaps(Vector{Vector{Int}}(undef, windows), [zeros(Int, haps) for i in 1:windows])
+UniqueHaplotypeMaps(windows::Int, haps::Int) = UniqueHaplotypeMaps(Vector{Vector{Int}}(undef, windows), [zeros(Int, haps) for i in 1:windows])
+
+# Each column is a person. Rows are sets storing redundant haplotypes for each window 
+struct PeoplesRedundantHaplotypeSet
+    p::Matrix{Set{Int}}
+end
+function PeoplesRedundantHaplotypeSet(windows::Int, people::Int) 
+    x = PeoplesRedundantHaplotypeSet(Matrix{Set{Int}}(undef, windows, people))
+    fill!(x.p, Set{Int}())
+    return x
+end
+Base.getindex(h::PeoplesRedundantHaplotypeSet, i::Int, j::Int) = h.p[i, j]
+
+
 
