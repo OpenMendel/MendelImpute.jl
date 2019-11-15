f24c09e2af8fc475d087d35db477f4a12e60ef82
Unknown <benchu99@hotmail.com>
2019-11-14 16:54:57 -0800

    added more simulations in notebook

--- simulation/msprime_script.py
+++ simulation/msprime_script.py
@@ -4,9 +4,9 @@ import msprime, sys
 sample_size = int(sys.argv[1])
 effective_population_size = int(sys.argv[2])
 sequence_length = int(sys.argv[3])
-seed = int(sys.argv[4])
-recombination_rate=2e-8
-mutation_rate=2e-8
+recombination_rate=float(sys.argv[4])
+mutation_rate=float(sys.argv[5])
+seed = int(sys.argv[6])
 
 # run the simulation
 tree_sequence = msprime.simulate(sample_size=sample_size, Ne=effective_population_size, length=sequence_length, recombination_rate=recombination_rate, mutation_rate=mutation_rate, random_seed=seed)
