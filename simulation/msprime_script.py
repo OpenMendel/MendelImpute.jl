2637c36b59f8ff12ba9f2b3e6ef1eb5c37f5b38f
Unknown <benchu99@hotmail.com>
2019-12-11 10:17:59 -0800

    initial comparison of beagle vs minimac vs mendel

--- simulation/msprime_script.py
+++ simulation/msprime_script.py
@@ -9,8 +9,11 @@ mutation_rate=float(sys.argv[5])
 seed = int(sys.argv[6])
 
 # run the simulation
-tree_sequence = msprime.simulate(sample_size=sample_size, Ne=effective_population_size, length=sequence_length, recombination_rate=recombination_rate, mutation_rate=mutation_rate, random_seed=seed)
+# ts = msprime.simulate(sample_size=sample_size, Ne=effective_population_size, length=sequence_length, recombination_rate=recombination_rate, mutation_rate=mutation_rate, random_seed=seed)
+ts = msprime.simulate(sample_size=sample_size, Ne=effective_population_size, length=sequence_length, recombination_rate=recombination_rate, random_seed=seed)
+model = msprime.InfiniteSites(msprime.NUCLEOTIDES)
+ts = msprime.mutate(ts, rate=mutation_rate, model=model, random_seed=seed)
 
 # print results
 with sys.stdout as vcffile:
-	tree_sequence.write_vcf(vcffile, 2) # 2 is for diploid
+	ts.write_vcf(vcffile, 2, position_transform="legacy") # 2 is for diploid, "legacy" = no matching positions
