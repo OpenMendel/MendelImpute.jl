1299cd9a993bbac5303fa6f34f1a6afabb434456
Unknown <benchu99@hotmail.com>
2020-04-18 23:56:17 -0700

    moved impute functions to impute.jl

--- src/impute.jl
+++ src/impute.jl
@@ -118,14 +118,14 @@ function impute_untyped(
     haplotypes::Int,
     )
     # write phase information to outfile
-    reader = VCF.Reader(openvcf(tgtfile, "r"))
+    reader = VCF.Reader(openvcf(reffile, "r"))
     writer = VCF.Writer(openvcf(outfile, "w"), header(reader))
-    pmeter = Progress(nrecords(tgtfile), 5, "Writing to file...")
+    pmeter = Progress(size(H, 1), 5, "Writing to file...")
     if chunks > 1
         # TODO
     else
         # loop over each record (snp)
-        for record in reader
+        for (i, record) in enumerate(reader)
             pos   = VCF.pos(record)
             gtkey = VCF.findgenokey(record, "GT")
             if !isnothing(gtkey) 
@@ -134,8 +134,8 @@ function impute_untyped(
                     # if missing = '.' = 0x2e
                     if record.data[geno[gtkey][1]] == 0x2e
                         #find where snp is located in phase
-                        hap1_position = searchsortedlast(phaseinfo[j].strand1.start, i)
-                        hap2_position = searchsortedlast(phaseinfo[j].strand2.start, i)
+                        hap1_position = searchsortedlast(phaseinfo[j].strand1.start, pos)
+                        hap2_position = searchsortedlast(phaseinfo[j].strand2.start, pos)
 
                         #find the correct haplotypes 
                         hap1 = phaseinfo[j].strand1.haplotypelabel[hap1_position]
@@ -158,6 +158,12 @@ function impute_untyped(
     flush(writer); close(reader); close(writer)
 end
 
+"""
+    update_marker_position!(phaseinfo, tgtfile)
+
+Converts `phaseinfo`'s strand1 and strand2's starting position in 
+terms of matrix rows to starting position in terms of SNP position. 
+"""
 function update_marker_position!(
     phaseinfo::Vector{HaplotypeMosaicPair},
     tgtfile::AbstractString,
@@ -179,4 +185,66 @@ function update_marker_position!(
             end
         end
     end
+end
+
+"""
+    impute!(X, H, phase)
+
+Imputes `X` completely using segments of haplotypes `H` where segments are stored in `phase`. 
+Non-missing entries in `X` can be different after imputation. 
+"""
+function impute!(
+    X::AbstractMatrix,
+    H::AbstractMatrix,
+    phase::Vector{HaplotypeMosaicPair}
+    )
+
+    fill!(X, 0)
+    # loop over individuals
+    for i in 1:size(X, 2)
+        for s in 1:(length(phase[i].strand1.start) - 1)
+            idx = phase[i].strand1.start[s]:(phase[i].strand1.start[s + 1] - 1)
+            X[idx, i] = H[idx, phase[i].strand1.haplotypelabel[s]]
+        end
+        idx = phase[i].strand1.start[end]:phase[i].strand1.length
+        X[idx, i] = H[idx, phase[i].strand1.haplotypelabel[end]]
+        for s in 1:(length(phase[i].strand2.start) - 1)
+            idx = phase[i].strand2.start[s]:(phase[i].strand2.start[s + 1] - 1)
+            X[idx, i] += H[idx, phase[i].strand2.haplotypelabel[s]]
+        end
+        idx = phase[i].strand2.start[end]:phase[i].strand2.length
+        X[idx, i] += H[idx, phase[i].strand2.haplotypelabel[end]]
+    end
+end
+
+"""
+    impute2!(X, H, phase)
+
+Imputes missing entries of `X` using corresponding haplotypes `H` via `phase` information. 
+Non-missing entries in `X` will not change. 
+"""
+function impute2!(
+    X::AbstractMatrix,
+    H::AbstractMatrix,
+    phase::Vector{HaplotypeMosaicPair}
+    )
+
+    p, n = size(X)
+
+    @inbounds for snp in 1:p, person in 1:n
+        if ismissing(X[snp, person])
+            #find where snp is located in phase
+            hap1_position = searchsortedlast(phase[person].strand1.start, snp)
+            hap2_position = searchsortedlast(phase[person].strand2.start, snp)
+
+            #find the correct haplotypes 
+            hap1 = phase[person].strand1.haplotypelabel[hap1_position]
+            hap2 = phase[person].strand2.haplotypelabel[hap2_position]
+
+            # imputation step 
+            X[snp, person] = H[snp, hap1] + H[snp, hap2]
+        end
+    end
+
+    return nothing
 end
