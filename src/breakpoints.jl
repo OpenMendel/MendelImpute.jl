2e852bd187062be150e3a70f5abd46e856dbd009
Unknown <benchu99@hotmail.com>
2020-05-13 00:34:56 -0700

    using l2 error for bkpt search is actually a bad idea due to high error

--- src/breakpoints.jl
+++ src/breakpoints.jl
@@ -1,16 +1,12 @@
-
 """
     continue_haplotype(X, H, happair_prev, happair_next)
-
 Find the optimal concatenated haplotypes from unordered haplotype pairs in two
 consecutive windows.
-
 # Input
 * `X`: an `n` vector of genotypes with {0, 1, 2} entries
 * `H`: an `n x d` reference panel of haplotypes with {0, 1} entries
 * `happair_prev`: unordered haplotypes `(i, j)` in the first window
 * `happair_next`: unordered haplotypes `(k, l)` in the second window
-
 # Output
 * `happair_next_optimal`: optimal ordered haplotypes in the second window
 * `breakpt`: break points in the ordered haplotypes
@@ -64,27 +60,26 @@ end
 
 """
     search_breakpoint(X, H, s1, s2)
-
 Find the optimal break point between s2[1] and s2[2] in configuration
 s1 | s2[1]
 s1 | s2[2]
 """
 function search_breakpoint(
-    X::AbstractVector{Union{Missing, T}},
+    X::AbstractVector,
     H::AbstractMatrix,
     s1::Int,
     s2::Tuple{Int, Int}
-    ) where T <: Real
+    )
 
     n = length(X)
     # count number of errors if second haplotype is all from H[:, s2[2]]
-    errors = zero(T)
+    errors = 0
     for pos in 1:n
         if !ismissing(X[pos])
-            errors += abs2(X[pos] - H[pos, s1] - H[pos, s2[2]])
+            errors += X[pos] ≠ H[pos, s1] + H[pos, s2[2]]
         end
     end
-    bkpt_optim, err_optim = 0, errors :: T
+    bkpt_optim, err_optim = 0, errors
 
     # quick return if perfect match
     err_optim == 0 && return 0, 0
@@ -92,85 +87,84 @@ function search_breakpoint(
     # extend haplotype H[:, s2[1]] position by position
     @inbounds for bkpt in 1:n
         if !ismissing(X[bkpt]) && H[bkpt, s2[1]] ≠ H[bkpt, s2[2]]
-            errors -= abs2(X[bkpt] - H[bkpt, s1] - H[bkpt, s2[2]])
-            errors += abs2(X[bkpt] - H[bkpt, s1] - H[bkpt, s2[1]])
-            if errors :: T < err_optim
+            errors -= X[bkpt] ≠ H[bkpt, s1] + H[bkpt, s2[2]]
+            errors += X[bkpt] ≠ H[bkpt, s1] + H[bkpt, s2[1]]
+            if errors :: Int < err_optim
                 bkpt_optim, err_optim = bkpt, errors
                 # quick return if perfect match
-                err_optim == 0 && return bkpt_optim, err_optim :: T
+                err_optim == 0 && return bkpt_optim, err_optim :: Int
             end
         end
     end
 
-    return bkpt_optim, err_optim :: T
+    return bkpt_optim, err_optim :: Int
 end
 
 """
     search_breakpoint(X, H, s1, s2)
-
 Find the optimal break point between s2[1] and s2[2] in configuration
 s1[1] | s2[1]
 s1[2] | s2[2]
 """
 function search_breakpoint(
-    X::AbstractVector{Union{Missing, T}},
+    X::AbstractVector,
     H::AbstractMatrix,
     s1::Tuple{Int, Int},
     s2::Tuple{Int, Int}
-    ) where T <: Real
+    )
 
-    err_optim   = typemax(T)
+    err_optim   = typemax(Int)
     bkpts_optim = (0, 0)
 
     # search over all combintations of break points in two strands
     @inbounds for bkpt1 in 0:length(X)
 
         # count number of errors if second haplotype is all from H[:, s2[2]]
-        errors = zero(T)
+        errors = 0
         for pos in 1:bkpt1
             if !ismissing(X[pos])
-                errors += abs2(X[pos] - H[pos, s1[1]] - H[pos, s2[2]])
+                errors += X[pos] ≠ H[pos, s1[1]] + H[pos, s2[2]]
             end
         end
         for pos in (bkpt1 + 1):length(X)
             if !ismissing(X[pos])
-                errors += abs2(X[pos] - H[pos, s1[2]] - H[pos, s2[2]])
+                errors += X[pos] ≠ H[pos, s1[2]] + H[pos, s2[2]]
             end
         end
-        if errors :: T < err_optim
+        if errors :: Int < err_optim
             err_optim = errors
             bkpts_optim = (bkpt1, 0)
 
             # quick return if perfect match
-            err_optim == 0 && return bkpts_optim, err_optim :: T
+            err_optim == 0 && return bkpts_optim, err_optim :: Int
         end
 
         # extend haplotype H[:, s2[1]] position by position
         for bkpt2 in 1:bkpt1
-            if !ismissing(X[bkpt2]) && H[bkpt2, s2[2]] != H[bkpt2, s2[1]]
-                errors -= abs2(X[bkpt2] - H[bkpt2, s1[1]] - H[bkpt2, s2[2]])
-                errors += abs2(X[bkpt2] - H[bkpt2, s1[1]] - H[bkpt2, s2[1]])
-                if errors :: T < err_optim
+            if !ismissing(X[bkpt2]) && H[bkpt2, s2[1]] ≠ H[bkpt2, s2[2]]
+                errors -= X[bkpt2] ≠ H[bkpt2, s1[1]] + H[bkpt2, s2[2]]
+                errors += X[bkpt2] ≠ H[bkpt2, s1[1]] + H[bkpt2, s2[1]]
+                if errors :: Int < err_optim
                     err_optim = errors
                     bkpts_optim = (bkpt1, bkpt2)
                 end
             end
         end
         for bkpt2 in (bkpt1 + 1):length(X)
-            if !ismissing(X[bkpt2]) && H[bkpt2, s2[2]] != H[bkpt2, s2[1]]
-                errors -= abs2(X[bkpt2] - H[bkpt2, s1[2]] - H[bkpt2, s2[2]])
-                errors += abs2(X[bkpt2] - H[bkpt2, s1[2]] - H[bkpt2, s2[1]])
-                if errors :: T < err_optim
+            if !ismissing(X[bkpt2]) && H[bkpt2, s2[1]] ≠ H[bkpt2, s2[2]]
+                errors -= X[bkpt2] ≠ H[bkpt2, s1[2]] + H[bkpt2, s2[2]]
+                errors += X[bkpt2] ≠ H[bkpt2, s1[2]] + H[bkpt2, s2[1]]
+                if errors :: Int < err_optim
                     err_optim = errors
                     bkpts_optim = (bkpt1, bkpt2)
                     # quick return if perfect match
-                    err_optim == 0 && return bkpts_optim, err_optim :: T
+                    err_optim == 0 && return bkpts_optim, err_optim :: Int
                 end
             end
         end
     end
 
-    return bkpts_optim, err_optim :: T
+    return bkpts_optim, err_optim :: Int
 end
 
 """
