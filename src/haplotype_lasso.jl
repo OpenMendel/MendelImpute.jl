93d4d34dec6590b84e7310a80a5cbeb45798290b
Unknown <benchu99@hotmail.com>
2020-07-04 16:46:23 -0700

    added hua's code for stepwise lasso selection

--- src/haplotype_lasso.jl
+++ src/haplotype_lasso.jl
@@ -1,66 +1,114 @@
 ######## THIS FILE IS THE SAME AS `haplotype_pair.jl`
-######## except it solves the least squares objective using lasso penalty
-######## || x - βH ||^2 + λ|| β ||_1 where we search λ so that || β ||_0 = 2 
+######## except it solves the least squares objective using a 
+######## heuristic stepwise search
 
 function haplopair_lasso(
     X::AbstractMatrix,
-    H::AbstractMatrix
+    H::AbstractMatrix,
+    r::Int = 1
     )
     
-    Xwork = zeros(Float32, size(X, 1), size(X, 2))
+    p, n  = size(X)
+    Xwork = zeros(Float32, p, n)
     Hwork = convert(Matrix{Float32}, H)
     initXfloat!(X, Xwork)
 
-    p, n     = size(X)
     happairs = ones(Int, n), ones(Int, n)
-    hapscore = zeros(Float32, n) # proportion of deviance explained for chosen model
+    hapscore = zeros(Float32, n)
 
-    haplopair_lasso!(Xwork, Hwork, happairs, hapscore)
-    t1 = t2 = t3 = t4 = 0 # no haplotype rescreening
+    # assemble M (symmetric)
+    t2 = @elapsed begin
+        M = zeros(Float32, d, d)
+        mul!(M, Transpose(H), H)
+        for j in 1:d, i in 1:(j - 1) # off-diagonal
+            M[i, j] = 2M[i, j] + M[i, i] + M[j, j]
+        end
+        for j in 1:d # diagonal
+            M[j, j] *= 4
+        end
+        LinearAlgebra.copytri!(M, 'U')
+    end
 
-    return happairs, hapscore, t1, t2, t3, t4
-end
-
-"""
-Finds λ so that only 2 β is non-zero using a bisection search.
-"""
-function haplopair_lasso!(
-    X::AbstractMatrix,
-    H::AbstractMatrix,
-    happairs::Tuple{AbstractVector, AbstractVector},
-    hapscore::AbstractVector
-    )
-
-    p, n, d = size(X, 1), size(X, 2), size(H, 2)
-    x = zeros(Float32, p)
-
-    for i in 1:n
-        copyto!(x, @view(X[:, i]))
-        
-        # find λ so that only 2 β is non-zero
-        λtop = 1.0
-        λlow = 0.0
-        linesearch = 0
-        while true
-            λmid = (λtop + λlow) / 2
-            path = fit(LassoPath, H, x, Normal(), IdentityLink(), λ=[λmid])
-
-            nz = count(!iszero, path.coefs)
-            if nz == 2 || (linesearch > 10 && nz > 1) # dangerous hack to bypass cases where nz is never 2
-                # record answer
-                hapscore[i] = path.pct_dev[1]
-                happairs[1][i] = path.coefs.rowval[1]
-                happairs[2][i] = path.coefs.rowval[2]
-                # println("λ = $λmid")
-                break
-            else
-                # bisection search
-                nz < 2 ? (λtop = λmid) : (λlow = λmid)
-                linesearch += 1
-            end
-            # println("λtop = $λtop, λlow = $λlow, λmid = $λmid, nz = $nz")
+    # assemble N
+    t2 += @elapsed begin
+        Nt = Transpose(Hwork) * Xwork
+        @simd for I in eachindex(Nt)
+            Nt[I] *= 2
         end
     end
 
+    t3 = 0
+    if r == 1
+        t3 += @elapsed haplopair_stepwise!(happairs[1], happairs[2], hapscore, M, Nt)
+    else 
+        t3 += @elapsed haplopair_topr!(happairs[1], happairs[2], hapscore, M, Nt, r = r)
+    end
+
+    t1 = t4 = 0 # no haplotype rescreening
+
+    return happairs, hapscore, t1, t2, t3, t4
+end 
+
+function haplopair_stepwise!(
+    happair1 :: AbstractVector{<:Integer},
+    happair2 :: AbstractVector{<:Integer},
+    hapmin   :: AbstractVector{T},
+    M        :: AbstractMatrix{T}, # d x d
+    Nt       :: AbstractMatrix{T}, # d x n
+    ) where T <: Real
+    d, n = size(Nt)
+    fill!(hapmin, typemax(T))
+    @inbounds for k in 1:n
+        # find the first haplotype
+        gmax, i1 = Nt[1, k], 1
+        for i in 2:d
+            g = Nt[i, k]
+            if g > gmax
+                gmax = g
+                i1   = i
+            end
+        end
+        happair1[k] = i1
+        # find the optimal second haplotype given i1 using LS criterion
+        for i in 1:d
+            score = M[i, i1] - Nt[i, k]
+            if score < hapmin[k]
+                hapmin[k] = score
+                happair2[k] = i
+            end
+        end
+        hapmin[k] -= Nt[i1, k]
+    end
+    return nothing
+end
+
+# same as haplopair_stepwise! but searches top `r` haplotypes with largest gradient
+function haplopair_topr!(
+    happair1 :: AbstractVector{<:Integer},
+    happair2 :: AbstractVector{<:Integer},
+    hapmin   :: AbstractVector{T},
+    M        :: AbstractMatrix{T}, # d x d
+    Nt       :: AbstractMatrix{T}, # d x n
+    r        :: Integer = 5
+    ) where T <: Real
+    d, n = size(Nt)
+    fill!(hapmin, typemax(T))
+    idx = Vector{Int}(undef, d)
+    @inbounds for k in 1:n
+        # find the top r haplotypes
+        @views sortperm!(idx, Nt[:, k]; alg = PartialQuickSort(r), rev = true)
+        # for each top haplotype, find the optimal second one
+        for riter in 1:r
+            i1 = idx[riter]
+            for i in 1:d
+                score = M[i, i1] - Nt[i1, k] - Nt[i, k]
+                if score < hapmin[k]
+                    hapmin[k] = score
+                    happair1[k] = i1
+                    happair2[k] = i
+                end
+            end
+        end
+    end
     return nothing
 end
