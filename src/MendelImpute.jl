2496e2aed9886e547a7e8a967222fed59c6ce365
Unknown <benchu99@hotmail.com>
2020-08-29 19:53:29 -0700

    drop dependency on statsbase

--- src/MendelImpute.jl
+++ src/MendelImpute.jl
@@ -2,10 +2,7 @@ __precompile__()
 
 module MendelImpute
 
-    import StatsBase: sample
-
     using LinearAlgebra
-    using StatsBase
     using GeneticVariation
     using VCFTools
     using SnpArrays
@@ -15,41 +12,11 @@ module MendelImpute
     using JLD2, FileIO, JLSO
     using Distances
 
-    export continue_haplotype, haplopair!, haplopair, haploimpute!
-    export impute!, impute_discard_phase!, search_breakpoint
-    export unique_haplotype_idx
-    export compute_redundant_haplotypes!, redundant_haplotypes
-    export HaplotypeMosaicPair, HaplotypeMosaic, UniqueHaplotypeMaps
-    export simulate_genotypes2
-    export compute_optimal_halotype_pair
-    export simulate_phased_genotypes
-    export connect_happairs
-    export phase!, phase_fast!, phase_sample!
-    export haplopair_screen!, haplopair_stepscreen!
-
     # main functions that users are exposed to
-    export phase
-    export compress_haplotypes
-    export nhaplotypes, nwindows, count_haplotypes_per_window
-    export avg_haplotypes_per_window, nchunks
-    export max_width
-
-    export compute_optimal_haplotypes!
-    export make_refvcf_file, make_tgtvcf_file
-    export simulate_uniform_haplotypes, simulate_markov_haplotypes
-    export simulate_genotypes
-    export unphase, compress_vcf_to_gz
-    export extract_marker_info
-
-    # imputation
-    export impute_typed_only!, impute_untyped!
-    export update_marker_position!
-
-    # chromosome painting
-    export paint, composition, unique_populations
-
-    # ultra-compressed
-    export convert_compressed
+    export phase               # main function for imputation and phasing
+    export compress_haplotypes # for compressing haplotype panels
+    export paint, composition  # chromosome painting and admix proportions
+    export convert_compressed  # for ultra-compression outputs
 
     include("compress.jl")
     include("data_structures.jl")
@@ -58,7 +25,6 @@ module MendelImpute
     include("haplotype_rescreen.jl")
     include("haplotype_thinning.jl")
     include("haplotype_stepscreen.jl")
-    include("simulate_utilities.jl")
     include("dynamic_programming.jl")
     include("impute.jl")
     include("breakpoints.jl")
