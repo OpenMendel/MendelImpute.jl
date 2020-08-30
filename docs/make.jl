0a510280b089ec1728f712054957e74317f79c18
Unknown <benchu99@hotmail.com>
2020-08-29 17:08:37 -0700

    add deploydoc command

--- docs/make.jl
+++ docs/make.jl
@@ -5,11 +5,16 @@ makedocs(
     sitename = "MendelImpute",
     format = Documenter.HTML(),
     modules = [MendelImpute]
+    pages = [
+        "Home" => "index.md",
+        "API"  => "man/api.md"
+    ]
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo   = "github.com/biona001/MendelImpute.jl.git",
+    target = "build"
+)
--- docs/src/man/api.md
+++ docs/src/man/api.md
@@ -17,8 +17,4 @@ paint
 composition
 unique_populations
 convert_compressed
-nhaplotypes
-nwindows
-count_haplotypes_per_window
-avg_haplotypes_per_window
 ```
